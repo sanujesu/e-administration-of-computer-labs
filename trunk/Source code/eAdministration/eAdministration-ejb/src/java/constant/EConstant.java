b5f04f37b7257719e69398a78d1149f7989fb49a
thesingle89@gmail.com <thesingle89@gmail.com@100cc374-72db-c6c8-91f0-58203e1ab6f0>
2012-09-19 17:55:46 +0000

    Tạo lớp để khai báo constant

---
+++ trunk/Source code/eAdministration/eAdministration-ejb/src/java/constant/EConstant.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package constant;
+
+/**
+ *
+ * @author Pakacoco
+ */
+public class EConstant {
+    public static String E_TODO_ID= "01";
+    public static String E_INPRO_ID= "02";
+    public static String E_VERIFY_ID= "03";
+    public static String E_DONE_ID= "04";
+    public static String E_SOURCE= "source";
+    public static String E_DEST= "dest";
+    public static String E_CONTENT= "content";
+
+
+
+}
