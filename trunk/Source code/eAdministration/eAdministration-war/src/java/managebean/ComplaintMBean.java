43911d31daa7a5806b52e6cd9335c6f8577b5892
thesingle89@gmail.com <thesingle89@gmail.com@100cc374-72db-c6c8-91f0-58203e1ab6f0>
2012-09-20 17:38:13 +0000

    Create managebean

---
+++ trunk/Source code/eAdministration/eAdministration-war/src/java/managebean/ComplaintMBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package managebean;
+
+
+/**
+ *
+ * @author Pakacoco
+ */
+public class ComplaintMBean {
+
+    /** Creates a new instance of ComplaintMBean */
+    public ComplaintMBean() {
+    }
+
+}
---
+++ trunk/Source code/eAdministration/eAdministration-war/src/java/managebean/TimetableMBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package managebean;
+
+
+/**
+ *
+ * @author Pakacoco
+ */
+public class TimetableMBean {
+
+    /** Creates a new instance of TimetableMBean */
+    public TimetableMBean() {
+    }
+
+}
