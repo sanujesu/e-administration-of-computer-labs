d82e7c6914fd9bd5060fe6557b5a686842c5ef9b
thesingle89@gmail.com <thesingle89@gmail.com@100cc374-72db-c6c8-91f0-58203e1ab6f0>
2012-09-20 17:23:45 +0000

    Create entity from database and create sessionbean

---
+++ trunk/Source code/eAdministration/eAdministration-ejb/src/java/sessionbean/TimetableSessionBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sessionbean;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Pakacoco
+ */
+@Stateless
+@LocalBean
+public class TimetableSessionBean {
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+ 
+}
