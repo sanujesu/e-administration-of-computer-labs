ebd16e0d233f3a1d2087d471ed0cc3d30211960d
thesingle89 <thesingle89@gmail.com>
2012-09-21 17:06:06 +0000

    delete, vi bi err chi k goi dc, tao lai thu xem

--- Source code/eAdministration/eAdministration-ejb/src/java/constant/EConstant.java
+++ Source code/eAdministration/eAdministration-ejb/src/java/constant/EConstant.java
@@ -10,13 +10,14 @@ package constant;
  * @author Pakacoco
  */
 public class EConstant {
-    public static String E_TODO_ID= "01";
-    public static String E_INPRO_ID= "02";
-    public static String E_VERIFY_ID= "03";
-    public static String E_DONE_ID= "04";
-    public static String E_SOURCE= "source";
-    public static String E_DEST= "dest";
+    public static String E_TODO_ID= "1";
+    public static String E_INPRO_ID= "2";
+    public static String E_VERIFY_ID= "3";
+    public static String E_DONE_ID= "4";
+    public static String E_OLD_STATUS= "oldStatus";
+    public static String E_NEW_STATUS= "newStatus";
     public static String E_CONTENT= "content";
+    public static String E_SEPERATOR= "_";
 
 
 
